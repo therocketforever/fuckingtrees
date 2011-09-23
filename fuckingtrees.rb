c3f273168ca24c3143ddd1266d3f6ffc4fcd2d27
DON who is a ghost <therocketforever@me.com>
2011-09-22 20:12:32 -0400

    Added some trees & some apples & some seeds.

--- fuckingtrees.rb
+++ fuckingtrees.rb
@@ -1,2 +1,31 @@
 require 'sinatra'
 require 'datamapper'
+
+DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/orchard.db")
+
+# Trees in the Orchard.
+class Tree
+  include DataMapper::Resource
+  property :id, Serial
+  
+  has n, :apples    # "a tree has many apples"
+  has n, :seeds, :through => :apples
+end
+
+# Apples on a Tree.
+class Apple
+  include DataMapper::Resource
+  property :id, Serial
+  
+  belongs_to :tree  # "an apple belongs to a tree..."
+  has_n, :seeds     # "...and has many seeds"
+end
+
+# Seeds in an Apple.
+class Seed
+  include DataMapper::Resource
+  property :id, Serial
+  
+  belongs_to :apple # "and a seed belongs to an apple"
+  
+DataMapper.finalize.auto_upgrade!
