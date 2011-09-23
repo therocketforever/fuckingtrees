3fada66fee73fb1abcd8a8c4a80acaacd12bbefd
DON who is a ghost <therocketforever@me.com>
2011-09-22 21:03:21 -0400

    individualized model names
    :tree_name
    :apple_name
    :seed_name

--- fuckingtrees.rb
+++ fuckingtrees.rb
@@ -7,6 +7,7 @@ DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/orchard.db")
 class Tree
   include DataMapper::Resource
   property :id, Serial
+  property :tree_name, String
 
   has n, :apples    # "a tree has many apples"
   has n, :seeds, :through => :apples
@@ -16,6 +17,7 @@ end
 class Apple
   include DataMapper::Resource
   property :id, Serial
+  property :apple_name, String
 
   belongs_to :tree # "an apple belongs to a tree..."
   has n, :seeds    # "...and has many seeds"
@@ -25,6 +27,7 @@ end
 class Seed
   include DataMapper::Resource
   property :id, Serial
+  property :seed_name, String
 
   belongs_to :apple  # "and a seed belongs to an apple"
 end
