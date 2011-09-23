16580f517e3ca088e60ecca634d78f8dfd5142be
DON who is a ghost <therocketforever@me.com>
2011-09-22 20:19:54 -0400

    Something was broke, now its not

--- fuckingtrees.rb
+++ fuckingtrees.rb
@@ -3,11 +3,11 @@ require 'datamapper'
 
 DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/orchard.db")
 
-# Trees in the Orchard.
+# Trees in the orchard.
 class Tree
   include DataMapper::Resource
   property :id, Serial
-  
+
   has n, :apples    # "a tree has many apples"
   has n, :seeds, :through => :apples
 end
@@ -16,17 +16,17 @@ end
 class Apple
   include DataMapper::Resource
   property :id, Serial
-  
-  belongs_to :tree  # "an apple belongs to a tree..."
-  has_n, :seeds     # "...and has many seeds"
+
+  belongs_to :tree # "an apple belongs to a tree..."
+  has n, :seeds    # "...and has many seeds"
 end
 
-# Seeds in an Apple.
+# Seeds in an Apple
 class Seed
   include DataMapper::Resource
   property :id, Serial
-  
-  belongs_to :apple # "and a seed belongs to an apple"
+
+  belongs_to :apple  # "and a seed belongs to an apple"
 end
-  
+
 DataMapper.finalize.auto_upgrade!
