619516b16846d1f704febc8fc223eec65b1a111c
DON who is a ghost <therocketforever@me.com>
2011-09-25 14:14:10 -0400

    Added some Sinatra stuff. Working Model.

--- fuckingtrees.rb
+++ fuckingtrees.rb
@@ -7,7 +7,7 @@ DataMapper::setup(:default, "sqlite3://#{Dir.pwd}/orchard.db")
 class Tree
   include DataMapper::Resource
   property :id, Serial
-  property :tree_name, String, :key => true
+  property :tree_name, String #, :key => true
 
   has n, :apples    # "a tree has many apples"
   has n, :seeds, :through => :apples
@@ -17,7 +17,7 @@ end
 class Apple
   include DataMapper::Resource
   property :id, Serial
-  property :apple_name, String, :key => true
+  property :apple_name, String #, :key => true
 
   belongs_to :tree # "an apple belongs to a tree..."
   has n, :seeds    # "...and has many seeds"
@@ -27,9 +27,19 @@ end
 class Seed
   include DataMapper::Resource
   property :id, Serial
-  property :seed_name, String, :key => true
+  property :seed_name, String #, :key => true
 
   belongs_to :apple  # "and a seed belongs to an apple"
 end
 
 DataMapper.finalize.auto_upgrade!
+
+get '/' do
+  @title = 'Orchard'
+  erb :orchard
+end
+
+get '/trees' do
+  @tree = Tree.get params[:id]
+  erb :trees
+end
